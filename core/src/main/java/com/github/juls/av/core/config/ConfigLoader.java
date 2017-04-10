ccf23f3a388e7003a386e27192070cac5c470211
fdesu <merkel05@gmail.com>
2017-04-10 19:55:10 +0200

    Move config into separate file

---
+++ core/src/main/java/com/github/juls/av/core/config/ConfigLoader.java
@@ -0,0 +1,18 @@
+package com.github.juls.av.core.config;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class ConfigLoader {
+
+    @Bean
+    public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+        ppc.setLocations(new ClassPathResource("core.config"));
+        return ppc;
+    }
+
+}
